2d94647e7390f08f2b33191c6f0fd8c57855d3d6
Phamthuyduyenn <1654052015duyen@ou.edu.vn>
2020-06-21 14:59:39 +0700

    hàm main

--- QuanLyThuVien/QuanLyThuVien/Trang chủ/GUIMain.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/Trang chủ/GUIMain.Designer.cs
@@ -114,7 +114,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 421);
+            this.ClientSize = new System.Drawing.Size(313, 446);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.MS);
             this.Controls.Add(this.DG);
