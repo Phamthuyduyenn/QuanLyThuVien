fef37de156d5dd7452d8784bb5e57de4d78b07a4
Phamthuyduyenn <1654052015duyen@ou.edu.vn>
2020-06-21 12:30:33 +0700

    Kết noosii CSDL

--- QuanLyThuVien/QuanLyThuVien/Đăng nhập/GUILogin.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/Đăng nhập/GUILogin.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnĐN = new System.Windows.Forms.Button();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(121, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 46);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Login !";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.Window;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Black;
+            this.btnExit.Location = new System.Drawing.Point(62, 277);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 35);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnĐN
+            // 
+            this.btnĐN.BackColor = System.Drawing.SystemColors.Window;
+            this.btnĐN.FlatAppearance.BorderSize = 0;
+            this.btnĐN.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnĐN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnĐN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnĐN.ForeColor = System.Drawing.Color.Black;
+            this.btnĐN.Location = new System.Drawing.Point(184, 277);
+            this.btnĐN.Margin = new System.Windows.Forms.Padding(2);
+            this.btnĐN.Name = "btnĐN";
+            this.btnĐN.Size = new System.Drawing.Size(106, 35);
+            this.btnĐN.TabIndex = 11;
+            this.btnĐN.Text = "Đăng nhập";
+            this.btnĐN.UseVisualStyleBackColor = false;
+            // 
+            // txtMK
+            // 
+            this.txtMK.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMK.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtMK.Location = new System.Drawing.Point(155, 217);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.PasswordChar = '*';
+            this.txtMK.Size = new System.Drawing.Size(135, 23);
+            this.txtMK.TabIndex = 10;
+            // 
+            // txtTK
+            // 
+            this.txtTK.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTK.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtTK.Location = new System.Drawing.Point(155, 168);
+            this.txtTK.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(135, 23);
+            this.txtTK.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(47, 218);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(47, 171);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tài khoản";
+            // 
+            // GUILogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(366, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnĐN);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "GUILogin";
             this.Text = "GUILogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnĐN;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- QuanLyThuVien/QuanLyThuVien/Đăng nhập/GUILogin.cs
+++ QuanLyThuVien/QuanLyThuVien/Đăng nhập/GUILogin.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
 
-namespace QuanLyThuVien.Đăng_nhập
+namespace LapTrinhCoSoDuLieu
 {
     public partial class GUILogin : Form
     {
+        DAOvsBUSLogin dn = new DAOvsBUSLogin();
+
         public GUILogin()
         {
             InitializeComponent();
         }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát chứ ?", "Thông báo", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                this.Close();
+        }
+
+        private void BtnĐN_Click(object sender, EventArgs e)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                string hash = dn.GetMd5Hash(md5Hash, txtMK.Text);
+
+                if (dn.kt(txtTK.Text, hash))
+                {
+                    MessageBox.Show("Đăng nhập thành công!", "Thông báo");
+                    txtTK.Clear();
+                    txtMK.Clear();
+                    txtTK.Focus();
+                    GUIMain main = new GUIMain();
+                    main.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Tài Khoản hoặc mật khẩu không hợp lệ!", "Thông báo");
+                    txtTK.Clear();
+                    txtMK.Clear();
+                    txtTK.Focus();
+                }
+            }
+        }
     }
 }
+
+
+
