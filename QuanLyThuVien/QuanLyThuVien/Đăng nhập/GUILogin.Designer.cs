9e361af619667f5e9977138365e0a2ca3330c8e5
Phamthuyduyenn <1654052015duyen@ou.edu.vn>
2020-06-21 12:24:06 +0700

    Thiết kế giao diện đăng nhập

--- QuanLyThuVien/QuanLyThuVien/Đăng nhập/GUILogin.Designer.cs
+++ QuanLyThuVien/QuanLyThuVien/Đăng nhập/GUILogin.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace QuanLyThuVien
+﻿namespace QuanLyThuVien.Đăng_nhập
 {
-    partial class Form1
+    partial class GUILogin
     {
         /// <summary>
         /// Required designer variable.
@@ -31,10 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "GUILogin";
         }
 
         #endregion
     }
 }
-
--- QuanLyThuVien/QuanLyThuVien/Đăng nhập/GUILogin.cs
+++ QuanLyThuVien/QuanLyThuVien/Đăng nhập/GUILogin.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuanLyThuVien
+namespace QuanLyThuVien.Đăng_nhập
 {
-    public partial class Form1 : Form
+    public partial class GUILogin : Form
     {
-        public Form1()
+        public GUILogin()
         {
             InitializeComponent();
         }
