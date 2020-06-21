769dcf9637ce74f31c3883fc5c42912a77eb2c39
Phamthuyduyenn <1654052015duyen@ou.edu.vn>
2020-06-21 12:39:15 +0700

    Thiết kế Độc giả

---
+++ QuanLyThuVien/QuanLyThuVien/Độc giả/GUIDocGia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThuVien.Độc_giả
+{
+    public partial class GUIDocGia : Form
+    {
+        public GUIDocGia()
+        {
+            InitializeComponent();
+        }
+    }
+}
